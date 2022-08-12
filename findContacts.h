fcd7926dd7bf3811464cdb73ffdb26cc9fdbaaf7
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-08-12 14:36:02 +0600

    For find any contact

---
+++ findContacts.h
@@ -0,0 +1,26 @@
+#include"addressBooks.h"
+#include"search.h"
+#pragma once
+
+void findContacts(AddressBooks *abooks) {
+    system("cls");
+    cout << "Enter Phone Number to find : ";
+    string phone;
+    cin >> phone;
+    int index = search(phone, abooks);
+    if(index > -1) {
+        cout << "           You Searched for : " << phone << endl;
+        cout << "\n\n           Contacts Found!\n\n";
+        cout << "          Name : " << abooks->contactsArray[index].name << endl;
+        cout << "          Gender : ";
+        if(abooks->contactsArray[index].gender == 1) cout << "Male" << endl;
+        else cout << "Female" << endl;
+        cout << "          Age : " << abooks->contactsArray[index].age << endl;
+        cout << "          Phone : " << abooks->contactsArray[index].phone << endl;
+        cout << "          Addrsss : " << abooks->contactsArray[index].address << endl;
+    }
+    else {
+        cout << "           You Searched for : " << phone << endl;
+        cout << "\n\n           Contacts Not Found!            " << endl;
+    }
+}
