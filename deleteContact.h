b67c06369dfdf97964b906df78c3c085cf9a3a36
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-08-12 14:35:44 +0600

    For deleting any contact

---
+++ deleteContact.h
@@ -0,0 +1,29 @@
+#include"addContacts.h"
+#include"search.h"
+#include"contacts.h"
+
+#pragma once
+
+void deleteContact(AddressBooks *abooks) {
+    system("cls");
+    string phone;
+    cout << "           Enter Phone Number to Delete : ";
+    cin >> phone;
+
+    int index = search(phone, abooks);
+    if(index > -1) {
+
+        Contacts temp = abooks->contactsArray[index];
+        for(int i = index; i <= abooks->countContacts - 1; i++) { 
+            abooks->contactsArray[i] = abooks->contactsArray[i+1];
+        }
+        abooks->countContacts -= 1;
+
+        cout << "           Contact with phone : " << temp.phone << " was deleted successfully!" << endl;
+    }
+    else {
+        system("cls");
+        cout << "           You Searched for : " << phone << endl << endl;
+        cout << "\n\n           Contacts Not Found!         \n\n";
+    }  
+}
