7c0a2825271c8ae5030f6283c0c35d105be1d4ec
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-08-12 14:32:40 +0600

    For showing main Menu

---
+++ menu.h
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+void showMenu() {
+    //system("cls");
+    cout << "\n\n---------- Contact Management System ----------\n\n";
+    cout << "1. Add Contact" << endl;
+    cout << "2. Display Contacts" << endl;
+    cout << "3. Find Contacts" << endl;
+    cout << "4. Modify Contacts" << endl;
+    cout << "5. Delete Contact" << endl;
+    cout << "6. Clear Contacts" << endl;
+    cout << "0. Exit" << endl;
+}
