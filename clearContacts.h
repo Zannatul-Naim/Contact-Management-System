ba5aeed28f48f6735cfb0ae5cbf476b39a4854d5
Zannatul-Naim <zannatulnaim09@gmail.com>
2022-08-12 14:35:24 +0600

    For remove all contacts

---
+++ clearContacts.h
@@ -0,0 +1,12 @@
+#include"addressBooks.h"
+
+void clearContacts(AddressBooks *abooks) {
+    cout << "\n\n           Are you sure want to delete all contacts(y\\n) : ";
+    char ch;
+    cin >> ch;
+    if(ch == 'y') {
+        abooks->countContacts = 0;
+
+        cout << "\n\n          All Contacts Cleared Successfully!" << endl;
+    }
+}
